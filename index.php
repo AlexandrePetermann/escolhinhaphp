318b8dafff5ffead9a8015aa2b59d3740e8ddee6
Alexandre Petermann <xandy.bq@gmail.com>
2015-11-17 22:37:53 -0200

    Inicialização do projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Escolinha PHP</title>
+    </head>
+    <body>      
+        <h1>Manuseio de Formulário com PHP</h1>
+        <h2>Referências:</h2>
+        <ul>
+            <li><a href="http://www.w3schools.com/php/" target="_back">W3Schools</a></li>
+            <li><a href="http://php.net/" target="_back" >Manual do PHP</a></li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
