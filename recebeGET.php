7d40f07573a6b89a070a24cf30b313a45115df22
Alexandre Petermann <xandy.bq@gmail.com>
2015-11-17 23:30:56 -0200

    Debug de código

--- recebeGET.php
+++ recebeGET.php
@@ -5,8 +5,19 @@
         <title>Recebe GET</title>
     </head>
     <body>
-        <h1>Exemplo de recebimento - Método GET</h1>
-        Bem-vindo <?php echo $_GET["name"]; ?><br>
-        Seu endereço de e-mail é <?php echo $_GET["email"]; ?>
+        <?php if (isset($_GET["name"])):
+            ?>
+            <h1>Exemplo de recebimento - Método GET</h1>
+            Bem-vindo <?php echo $_GET["name"] ?><br>
+            Seu endereço de e-mail é <?php echo $_GET["email"]; ?>
+            <?php
+        endif;
+        ?>
+        <pre> <!--Debug-->
+            <?php
+            var_dump($_GET);
+            ?>
+        </pre>
+
     </body>
 </html>
