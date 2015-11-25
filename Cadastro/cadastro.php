722e5ba9200c0d817142be0cc56183d02d183a0c
Alexandre <xandy.bq@gmail.com>
2015-11-25 06:35:06 -0200

    Exclusão de dados

--- Cadastro/cadastro.php
+++ Cadastro/cadastro.php
@@ -18,16 +18,13 @@ try {
 
 // Verifica se foi enviado através do botão
 If (isset($_POST['btn'])) {
-    /*
-      Recepção de dados
-     */
-//    echo "<h1>$_POST[email]</h1>";
-    // Verifica se foi preenchido o e-mail
+// Inserção de dados
+// Verifica se foi preenchido o e-mail
     if (isset($_POST['email']) && !empty($_POST['email'])) {
-        // FIltragem de entrada de dados
+// Filtragem de entrada de dados
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
         $cod = gerarCodigo();
-        // String SQL
+// String SQL
         $sql = "Insert Into lista (email,codigo,datacadastro) values(:email,:cod,now())";
         $parametros = array(':email' => $email,
             ':cod' => $cod);
@@ -35,20 +32,35 @@ If (isset($_POST['btn'])) {
         $q = $p->execute($parametros);
     }
 } elseif (isset($_GET['cod'])) {
-    // Listagem de dados
-    $sql = "select email, situacao, datacadastro from lista";
-    $q = $conn->query($sql);
-    $q->setFetchMode(PDO::FETCH_ASSOC);
-    while ($r = $q->fetch()) {
-        echo "<p>";
-        echo $r['email'] . "\t";
-        echo $r['situacao'] . "\t";
-        echo $r['datacadastro'] . "\t";
+    IF ($_GET['cod'] == 'listar') {
+        // Listagem de dados
+        $sql = "select email, situacao, datacadastro, codigo from lista";
+        $q = $conn->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        while ($r = $q->fetch()) {
+            echo "<p style='color:";
+            echo $r['situacao'] ? 'green' : 'red';
+            echo ";'>";
+            echo $r['email'] . "\t";
+            echo $r['situacao'] . "\t";
+            echo $r['datacadastro'] . "\t";
+            echo "<a href='cadastro.php?cod=d&hash=$r[codigo]' title='Clique para excluir'>"; // Link de exclusão
+            echo $r['codigo'];
+            echo "</a>";
+            echo "</p>";
+        }
+    } elseIF ($_GET['cod'] == 'd' && isset($_GET['hash'])) {
+        // Exclusão de dados
+        $sql = 'delete from lista where codigo = :hash';
+        $hash = filter_input(INPUT_GET, 'hash', FILTER_SANITIZE_STRING);
+        $p = $conn->prepare($sql);
+        $q = $p->execute(array(':hash' => $hash));
+        echo "Excluido o e-mail!";
+        header("Location: cadastro.php?cod=listar");
     }
-    // Exclusão de dados
-    // validação do e-mail
+// validação do e-mail
 } else {
-    // botão cadastrar não foi pressionado
-    // redirecionada para a pagina inicial
+// botão cadastrar não foi pressionado
+// redirecionada para a pagina inicial
     header('Location: index.php');
 }
